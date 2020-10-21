89a862699e908d3b39c5343f0906b2714dc9b996
xxglt <xxglt9094@163.com>
2020-10-21 15:37:16 +0800

    01-首次提交

---
+++ src/router/index.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+import vueRouter from 'vue-router';
+Vue.use(vueRouter);
+const router = new vueRouter({
+  // 在vue实例中配置路由规则
+  routes: [{ path: '' }],
+});
+// 导出路由
+export default router;
